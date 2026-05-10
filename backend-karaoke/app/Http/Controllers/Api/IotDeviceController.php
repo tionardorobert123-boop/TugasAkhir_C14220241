2bf48584798507551733ae72e25bb96eff97eb2e
bertt11 <131933705+bertt11@users.noreply.github.com>
2026-05-10 22:01:13 +0800

    setup gateway sync cloud

--- backend-karaoke/app/Http/Controllers/Api/IotDeviceController.php
+++ backend-karaoke/app/Http/Controllers/Api/IotDeviceController.php
@@ -10,36 +10,45 @@ class IotDeviceController extends Controller
 {
     public function sync(Request $request)
     {
-        $device = IotDevice::where(
-            'room_id',
-            $request->room_id
-        )->first();
+        try {
 
-        if (!$device) {
+            $device = IotDevice::where(
+                'device_id',
+                $request->room_id
+            )->first();
+
+            if (!$device) {
+
+                return response()->json([
+                    'message' => 'Device not found'
+                ], 404);
+            }
+
+            $device->door_status =
+                $request->door_status;
+
+            $device->lock_status =
+                $request->lock_status;
+
+            $device->status_online =
+                $request->status_online;
+
+            $device->last_seen =
+                now();
+
+            $device->miss_count = 0;
+
+            $device->save();
 
             return response()->json([
-                'message' => 'Device not found'
-            ], 404);
+                'message' => 'IoT synced'
+            ]);
+
+        } catch (\Throwable $e) {
+
+            return response()->json([
+                'error' => $e->getMessage()
+            ], 500);
         }
-
-        $device->door_status =
-            $request->door_status;
-
-        $device->lock_status =
-            $request->lock_status;
-
-        $device->status_online =
-            $request->status_online;
-
-        $device->last_seen =
-            now();
-
-        $device->miss_count = 0;
-
-        $device->save();
-
-        return response()->json([
-            'message' => 'IoT synced'
-        ]);
     }
 }
