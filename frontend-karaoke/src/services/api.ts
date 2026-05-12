3c3c3754d0a04bf91864413c817bddbaa3db2f69
bertt11 <131933705+bertt11@users.noreply.github.com>
2026-05-12 14:57:02 +0800

    UI kasir updated

--- frontend-karaoke/src/services/api.ts
+++ frontend-karaoke/src/services/api.ts
@@ -6,24 +6,54 @@ const LOCAL_API =
 const CLOUD_API =
   "https://tugasakhirc14220241.up.railway.app/api";
 
-const API = axios.create();
-
-API.interceptors.request.use((config) => {
-
-  // SWITCH API
-  config.baseURL =
-    navigator.onLine
-      ? CLOUD_API
-      : LOCAL_API;
-
-  // TOKEN
-  const token =
-    localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization =
-      `Bearer ${token}`;
-  }
-  return config;
+const API = axios.create({
+  timeout: 5000,
 });
+
+// DETECT API
+async function getAvailableAPI() {
+
+  try {
+
+    await axios.get(
+      `${CLOUD_API}/ping`,
+      {
+        timeout: 2000,
+      }
+    );
+
+    return CLOUD_API;
+
+  } catch (error) {
+
+    console.log(
+      "Cloud unavailable, switch local API"
+    );
+
+    return LOCAL_API;
+  }
+}
+
+// INTERCEPTOR
+API.interceptors.request.use(
+  async (config) => {
+
+    // AUTO SWITCH
+    config.baseURL =
+      await getAvailableAPI();
+
+    // TOKEN
+    const token =
+      localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default API;
