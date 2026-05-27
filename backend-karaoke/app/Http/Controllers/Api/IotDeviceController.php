24d29af111e2d17c8c636b78113b85434e384b32
roberttionardo <tionardorobert@gmail.com>
2026-05-27 11:35:38 +0700

    retry esp

--- backend-karaoke/app/Http/Controllers/Api/IotDeviceController.php
+++ backend-karaoke/app/Http/Controllers/Api/IotDeviceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\IoTDevice;
+use Illuminate\Support\Facades\DB;
 
 class IotDeviceController extends Controller
 {
