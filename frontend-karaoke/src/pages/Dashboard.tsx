d4c1ee255e86b52f9f7037e0f21b82cec9a91f8e
roberttionardo <tionardorobert@gmail.com>
2026-06-01 10:29:31 +0700

    sync ui

--- frontend-karaoke/src/pages/Dashboard.tsx
+++ frontend-karaoke/src/pages/Dashboard.tsx
@@ -8,6 +8,7 @@ import { useNavigate } from "react-router-dom";
 
 import logo from "../assets/logo.png";
 import RoomSettingModal from "../components/Rooms/RoomSettingModal";
+import { useCloud } from "../context/CloudContext";
 
 function Dashboard() {
   const {
@@ -60,6 +61,15 @@ function Dashboard() {
 
   const selectedExtendRoom = rooms.find(r => r.room_id === extendRoomId);
   const extendPrice = selectedExtendRoom?.price_per_hour ?? 0;
+
+  const {
+  syncInfo
+} = useCloud()
+
+console.log(
+  'SYNC INFO',
+  syncInfo
+)
 
   const renderRoom = (id: number) => {
     const room = rooms.find((r) => r.room_id === id);
