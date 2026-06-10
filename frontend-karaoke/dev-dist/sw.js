e3d956709aa4fd309c8b529f2a805457c7bd87f8
tionardorobert123-boop <tionardorobert123@gmail.com>
2026-06-10 09:21:25 +0700

    t

--- frontend-karaoke/dev-dist/sw.js
+++ frontend-karaoke/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-970124e6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vt7npkvu8l"
+    "revision": "0.qms51jschbc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
