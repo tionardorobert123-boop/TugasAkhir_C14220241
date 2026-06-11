566fc7229ec7a6c18d5decb6066d1f1c4b20916c
tionardorobert123-boop <tionardorobert123@gmail.com>
2026-06-11 22:57:43 +0700

    t

--- backend-karaoke/app/Models/User.php
+++ backend-karaoke/app/Models/User.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Models;
-
-use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
